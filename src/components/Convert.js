378ce3f9af7632f9598420437ed3b5ac03d96338
sasha <sasha@debianmain>
2021-05-07 18:40:17 +0300

    comment

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,13 +1,24 @@
 //AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM  //from Udemy
-//AIzaSyCo3L_PsxnTrxt5xbbweuntCcoWME_gZvQ  //my
 
 import React, { useState, useEffect } from "react";
 import Translate from "./Translate";
 import axios from "axios";
 // do not forget about {language,text}
+// you may have several useEffect function, no problem
 const Convert = ({ language, text }) => {
   //useEffect will be runnig if language and text will be changed
   const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 700);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
   useEffect(() => {
     const doTranstaltion = async () => {
       const { data } = await axios.post(
@@ -15,7 +26,7 @@ const Convert = ({ language, text }) => {
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
           },
@@ -24,7 +35,7 @@ const Convert = ({ language, text }) => {
       setTranslated(data.data.translations[0].translatedText);
     };
     doTranstaltion();
-  }, [language, text]);
+  }, [language, debouncedText]);
 
   return <h1>{translated}</h1>;
 };
