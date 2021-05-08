6e153a41179c717173d152aa6260c4ab02206c90
sasha <sasha@debianmain>
2021-05-08 18:25:52 +0300

    comment

---
+++ src/Glossary.js
@@ -0,0 +1,9 @@
+//this project glossary
+// A page is requestion a server and server sends HTML document.
+// It is not normal with React app.
+// For example:
+// User clicks on 'List'
+// 1) Change the URL, but dont do a full page refresh@
+// 2) Each Route could detect the URL has changed
+// 3) Route could update the piece of state tracking the current pathname
+// 4) Each route rerenders, showing/hiding components appropriately
